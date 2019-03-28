dbc46d638660d7969982aeac28df102f19abeb3c
Artem Teluschenko <32033819+Tellirrium@users.noreply.github.com>
2019-03-28 20:54:14 +0300

    Update script.js

--- Lesson 13 - HomeWork/task1 - Chess board/script.js
+++ Lesson 13 - HomeWork/task1 - Chess board/script.js
@@ -1,66 +1,54 @@
 function createGrid() {
+	var x = document.getElementById('x');
+	var xValue = x.addEventListener( "input", function(){} );
 
-var x = document.getElementById('x');
-var xValue = x.addEventListener( "input", function(){} );
+	var y = document.getElementById('y');
+	var yValue = y.addEventListener( "input", function(){} );
 
-var y = document.getElementById('y');
-var yValue = y.addEventListener( "input", function(){} );
-
-var table = document.getElementById('tbody');
+	var table = document.getElementById('tbody');
 
 
-var a = +x.value;
-var b = +y.value;
+	var a = +x.value;
+	var b = +y.value;
 
-if (a < 10 && b < 10) {
+	if (a < 10 && b < 10) {
+		for (var i = 0; i < b; i++) {
+			
+		var tr = document.createElement('tr');
+		table.appendChild(tr);
 
-for (var i = 0; i < b; i++) {
+    			for (var j = 0; j < a; j++) {
 
-	var tr = document.createElement('tr');
-
-	table.appendChild(tr);
-
-    for (var j = 0; j < a; j++) {
-
-        var ourTd = table.getElementsByTagName('tr')[i];
-        var td = document.createElement('td');
+			var ourTd = table.getElementsByTagName('tr')[i];
+			var td = document.createElement('td');
         
-        ourTd.appendChild(td);
+        		ourTd.appendChild(td);
+			}
+		}
+		
+	} else {
+		
+    		alert('Error. You can use only numbers, which less than 10');
 	}
-}
-} else {
-    alert('Error. You can use only numbers, which less than 10');
-}
 
-button.removeEventListener("click", createGrid);
-
+	button.removeEventListener("click", createGrid);
 }
 
 var button = document.getElementById('btn');
 
 button.addEventListener("click", createGrid);
 
-
 var table = document.getElementById('tbody');
 table.addEventListener("click", changeGrid);
 
-
 function changeGrid() {
 	if ( this.classList.contains('tabl1') ) {
+		table.classList.add('tabl2');
+		table.classList.remove('tabl1');
 
-	table.classList.add('tabl2');
-    table.classList.remove('tabl1');
+	} else if ( this.classList.contains('tabl2') ) {
+		table.classList.add('tabl1');
+		table.classList.remove('tabl2');
 
-} else if ( this.classList.contains('tabl2') ) {
-
-	table.classList.add('tabl1');
-    table.classList.remove('tabl2');
-
+	}
 }
-}
-
-
-
-
-
-
