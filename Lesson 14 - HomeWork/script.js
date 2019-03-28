dc4ed77e7c5407f261b90f2d79a2fe983b382796
Artem Teluschenko <artem.suvazu@gmail.com>
2019-03-28 12:42:10 +0300

    Third commit

--- Lesson 14 - HomeWork/script.js
+++ Lesson 14 - HomeWork/script.js
@@ -21,12 +21,13 @@ function request() {
         users = JSON.parse(this.response).data;
         var length = users.length;
         for (var i = 0; i < length; i++) {
+            counter = (i + 1);
             var userfon = document.createElement('div');
             userbtns.appendChild(userfon);
             var userdiv = userbtns.getElementsByTagName('DIV')[i];
             userdiv.innerHTML = 'User ' + (i + 1);
             userdiv.setAttribute('class', 'userbutton');
-
+            userdiv.setAttribute('id', 'btn' + counter);
             // var localInfo = users[i];
             // var src = localInfo['avatar'];
             // var fn = localInfo['first_name'];
@@ -50,34 +51,40 @@ function request() {
             var src = localInfo['avatar'];
             var fn = localInfo['first_name'];
             var ln = localInfo['last_name'];
-            counter = (i + 1);
 
             var userinfo = document.createElement('div');
             info.appendChild(userinfo);
             var userinforeal = info.getElementsByTagName('DIV')[i];
-            userinforeal.setAttribute('id', counter);
+            userinforeal.setAttribute('id', 'div' + counter);
             userinforeal.innerHTML = '<img></img>' + ' ' + 'First name ' + fn + ' ' + 'Last name ' + ln;
             var img = userinforeal.getElementsByTagName('IMG')[0];
             img.setAttribute('src', src);
         }
-        var div1 = document.getElementById('1');
-        var div2 = document.getElementById('2');
-        var div3 = document.getElementById('3');
+
+        var div1 = document.getElementById('div1');
+        div1.classList.add('block');
+        var div2 = document.getElementById('div2');
+        div2.classList.add('none');
+        var div3 = document.getElementById('div3');
+        div3.classList.add('none');
+        var btn1 = document.getElementById('btn1');
+        var btn2 = document.getElementById('btn2');
+        var btn3 = document.getElementById('btn3');
 
         userbtns.onclick = function(event) {
             var target = event.target;
-            if (target == div2) {
-                div2.className = 'block';
-                div3.className = 'none';
-                div1.className = 'none';
-            } else if (target == div3) {
-                div3.className = 'block';
-                div1.className = 'none';
-                div2.className = 'none';
-            } else if (target == div1) {
-                div1.className = 'block';
-                div3.className = 'none';
-                div2.className = 'none';
+            if (target == btn2) {
+                div3.className ='none';
+                div1.className ='none';
+                div2.className ='block';
+            } else if (target == btn3) {
+                div2.className ='none';
+                div1.className ='none';
+                div3.className ='block';
+            } else if (target == btn1) {
+                div2.className ='none';
+                div3.className ='none';
+                div1.className ='block';
             }
 
         }
